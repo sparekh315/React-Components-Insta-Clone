334e0ae276ef8a3334ceafe38776a4b061c17cf2
sparekh315 <saniaparekh315@gmail.com>
2020-06-26 00:30:38 -0500

    completed react insta clone components, and functionality

--- src/components/CommentSection/CommentSectionContainer.js
+++ src/components/CommentSection/CommentSectionContainer.js
@@ -6,13 +6,17 @@ import "./Comment.css";
 
 const CommentSection = props => {
   // Add state for the comments
+  const [comments]= useState(props.comments)
+    return (
+      <div>
+        {/* map through the comments data and return the Comment component */}
+         {comments.map(commentData =>
+           <Comment  comment={commentData} />
+           )};
+        <CommentInput/>
+      </div>
+    );
+  }
 
-  return (
-    <div>
-      {/* map through the comments data and return the Comment component */}
-      <CommentInput />
-    </div>
-  );
-};
 
 export default CommentSection;
--- src/components/PostsContainer/PostsPage.js
+++ src/components/PostsContainer/PostsPage.js
@@ -1,14 +1,21 @@
 //Complete the necessary code in this file
 // import useState
-import React from "react";
+import React, {useState} from "react";
 import Post from "./Post";
 import "./Posts.css";
 // import data 
+import data from '../../dummy-data';
+import dummyData from "../../dummy-data";
 
 const PostsPage = () => {
   // set up state for your data
+  const [data, setData] = useState(dummyData)
+  console.log(data);
   return (
     <div className="posts-container-wrapper">
+    {data.map(postData => 
+      <Post  post={postData} />
+    )};
       {/* map through data here to return a Post and pass data as props to Post */}
     </div>
   );
